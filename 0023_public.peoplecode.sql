d0b62e39dc3329d8acf8b1ff9da5a98442857815
John Rogers <jrogers@strategiaanalytics.com>
2019-11-22 10:12:48 +0000

    Auto commit

--- 0023_public.peoplecode.sql
+++ 0023_public.peoplecode.sql
@@ -29,6 +29,7 @@ COPY public.peoplecode (idcode, idpeople, status, codehash, state, isadmin) FROM
 22	16	0	5OyXZpFkvwe0XWfAbg1jWq3P3yfPgpKcHdBPF8	confirmed	f
 23	17	1	wRvLhX7fSZB4lX5QHTXgMcw4BuGlb6335Vdawa	confirmed	f
 24	18	1	GCIKPeGnMvOBbZQBlMPDuTnwWWayuCGjA5dgPN	unconfirmed	f
+25	6	1	GCIKPeGnMvOBbZQBlMPDuTnwWWayuCGjA5dgPN	confirmed	t
 \.
 
 
@@ -36,4 +37,4 @@ COPY public.peoplecode (idcode, idpeople, status, codehash, state, isadmin) FROM
 -- Name: peoplecode_id_seq; Type: SEQUENCE SET; Schema: public; Owner: adminstrategia
 --
 
-SELECT pg_catalog.setval('public.peoplecode_id_seq', 24, true);
+SELECT pg_catalog.setval('public.peoplecode_id_seq', 25, true);
