f9c64a5b929a5f698d2a1c3fa680f71dd8d9dc7f
John Rogers <jrogers@strategiaanalytics.com>
2020-08-06 10:12:50 +0000

    Auto commit

--- 0001_public.assessment.sql
+++ 0001_public.assessment.sql
@@ -69,7 +69,7 @@ COPY public.assessment (idassessment, idassessmenttype, idproject, name, startda
 99	1	28	OSA 2020-04	2020-04-14 00:00:00	2020-05-14 00:00:00	2	3	AA04
 100	1	7	OSA 2020-04	2020-04-14 00:00:00	2020-04-25 00:00:00	2	20	CoC420
 101	1	31	Company's employees	2020-07-13 00:00:00	2020-07-30 00:00:00	2	20	kitchen
-102	1	33	OSA	2020-08-06 00:00:00	2020-08-19 00:00:00	0	25	Crowley
+102	1	33	OSA	2020-08-04 00:00:00	2020-08-19 00:00:00	1	25	Crowley
 \.
 
 
